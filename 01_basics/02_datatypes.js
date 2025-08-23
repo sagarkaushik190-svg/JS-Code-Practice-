079a1d69a699bb47fa459080651e00788a3a2e32
sagarkaushik190-svg <sagarkaushik190@gmail.com>
2025-08-23 09:56:17 +0000

    Configure done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"Use strict";        // Treat all JS code to newer version.
+// alert(3 + 3);    // We are using nodejs not browser.
+
+let Name = "Sagar Kaushik";
+console.log(typeof Name);
+
+/*
+num => 2 to power of 53
+bigint =>
+string => "Sagar"
+boolean => True / False
+null => standalone value
+undefined =>
+symbol => unique
+object =>
+*/
+
+console.log(typeof null);                         // Object
+console.log(typeof undefined);                    // undefined
+
