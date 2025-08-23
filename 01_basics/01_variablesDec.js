fd1af463aeea27c247a100629e7ee89e4f5a9480
sagarkaushik190-svg <sagarkaushik190@gmail.com>
2025-08-23 09:25:37 +0000

    Configure done

---
+++ 01_basics/01_variablesDec.js
@@ -0,0 +1,9 @@
+const AccntId = 12345;
+var EmailId = "sagarkaushik190@gmail.com";
+let Name = "Sagar";
+
+console.table([AccntId,EmailId,Name]);
+
+/*
+Prefer not to use var bcoz of issue in block scope and functional scope. 
+*/
