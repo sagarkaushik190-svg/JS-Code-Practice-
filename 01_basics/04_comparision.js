22dc0fbf6cb37eb0d911e9bd90c5cb61b8dddb5a
sagarkaushik190-svg <sagarkaushik190@gmail.com>
2025-08-30 18:14:52 +0000

    Configure done

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,7 @@
+console.log(3 > 2);
+console.log("3" > 2);
+console.log(true > 2);
+
+console.log("3" >= 2);
+console.log(null >= 2);
+console.log(undefined >= 2);
---
+++ 05_datatype_summary.js
@@ -0,0 +1,11 @@
+// Primitive data types : Number, string, bigInt, null, undefined, symbol, boolean.
+
+const num = 2;
+// const str = "Sagar";
+// const bigInt = 444444555556273893223739n;   
+// const nu = null;
+// let email;
+// const bool = true;
+// const id = Symbol('123');
+
+console.log(typeof num);
