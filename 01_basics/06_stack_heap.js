b55c0b50591589e8ba2ffef6c42d0d42b3e8b0ef
sagarkaushik190-svg <sagarkaushik190@gmail.com>
2025-08-31 07:31:06 +0000

    stack and heap

---
+++ 01_basics/06_stack_heap.js
@@ -0,0 +1,18 @@
+const stack1 = "Sagar Kaushik";
+var stack2 = stack1;             // Assign value to another variable.
+
+var stack2 = "Harshita Kaushik"; // Assingn another value.
+
+console.log(stack2);             // Stack: The only change in the copy value not the original.
+console.log(stack1);
+
+let user1 = {
+    Name : "Taniya Bhardwaj",
+    age : 26
+}
+
+var user2 = user1;
+user2.age = 34;           // Change done in the original value. 
+ 
+console.log(user2);    // Heap: Only change the original value not copy value as compare to stack. 
+console.log(user1);
