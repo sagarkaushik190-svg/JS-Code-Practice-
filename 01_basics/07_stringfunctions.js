e108c56f089877827d2bd1abed2ce2c725bd3a85
sagarkaushik190-svg <sagarkaushik190@gmail.com>
2025-08-31 14:25:42 +0000

    string function

---
+++ 01_basics/07_stringfunctions.js
@@ -0,0 +1,12 @@
+const str1 = "                Sagar              ";
+const str2 = "Kaushik";
+
+console.log(`hey my name is: ${str1}`);  // Bactics method.
+
+console.log(str1.concat(str2));  // adding 2 strings.
+console.log(str1.trim());       // remove whitespace.
+console.log(str1.replace("Sagar","Harshita")); // Replace old string value. 
+console.log(str1.toUpperCase())  // Uppercase 
+console.log(str2.charAt(1));     // return the character at the specific index. 
+console.log(str2.indexOf("u"));    // return index of the character. 
+
