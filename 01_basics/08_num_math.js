8109504679a15ec32b1299ebd6d6b4dd1c1771f9
sagarkaushik190-svg <sagarkaushik190@gmail.com>
2025-08-31 16:45:27 +0000

    num maths

---
+++ 01_basics/08_num_math.js
@@ -0,0 +1,28 @@
+// const num1 = 20;
+// const num2 = 10;
+
+// console.log(num1 + num2);      // addition
+// console.log(num1 - num2);     // subtraction
+// console.log(num1 * num2);    // multiply
+// console.log(num1 / num2);   // divide
+// console.log(num1 ** num2); // exponentiation
+// console.log(num1 % num2); // modulus (Remainder)
+// // ++	Increment
+// // --	Decrement
+
+
+// //*********************Math*********************************** */
+
+// console.log(Math.min(num1,num2));
+// console.log(Math.max(num1,num2));
+
+// console.log(Math.random() * 10) + 1;
+
+
+// console.log(Math.floor(Math.random() * 10) + 1);
+
+const max = 20;
+const min = 6;
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min));
+
