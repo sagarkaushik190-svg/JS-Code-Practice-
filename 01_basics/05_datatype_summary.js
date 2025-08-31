f486ea19a99abb45768245cbdc6f00fdde11053f
sagarkaushik190-svg <sagarkaushik190@gmail.com>
2025-08-31 07:14:36 +0000

    Configure done

---
+++ 01_basics/05_datatype_summary.js
@@ -0,0 +1,25 @@
+// Primitive data types : Number, string, bigInt, null, undefined, symbol, boolean.
+
+const score = 33; 
+const str = "Sagar";
+const bigInt = 444444555556273893223739n;   
+const nu = null;
+let email;
+const bool = true;
+const id = Symbol('123');
+
+console.log(typeof email);
+
+// Refrence (Non-Primitive): array,functions,object
+
+const names = ["sagar", "harshita"];
+
+const obj = {
+    name : "Sagar",
+    age : 29
+}
+
+const fun = function(){
+
+}
+console.log(typeof names);
