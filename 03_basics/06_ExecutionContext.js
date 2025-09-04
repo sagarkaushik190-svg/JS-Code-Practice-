745893eda893fe83b5d51e12899d0e56a8ebe782
sagarkaushik190-svg <sagarkaushik190@gmail.com>
2025-09-04 09:53:00 +0000

    ec

--- 03_basics/06_ExecutionContext.js
+++ 03_basics/06_ExecutionContext.js
@@ -9,5 +9,5 @@ function sum(num1,num2){
 let result1 = sum(var1,var2);
 let result2 = sum(20,20);
 
-console.log(result1);
-console.log(result2);
+console.log("Result1 is: ", result1);
+console.log("Result2 is: ", result2);
