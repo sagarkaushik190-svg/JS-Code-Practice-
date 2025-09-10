bc81c4ce1c9f8d3d304b3d71f753a7aeec7e18ad
sagarkaushik190-svg <sagarkaushik190@gmail.com>
2025-09-10 08:49:50 +0000

    ifelse

---
+++ 04_controlFlow/ifelse.js
@@ -0,0 +1,22 @@
+// let score = 200;
+
+// if(score < 100){
+//     console.log("Score is less.");    
+// } else {
+//     console.log("Score is greater.")
+// } 
+
+
+let userLoggedIn = true;
+let userLoggedInOffice = false;
+
+if (userLoggedIn && userLoggedInOffice) {
+    console.log("Great Employee.");   
+} else {
+    console.log("Need to change the Employee.");
+}
+if (userLoggedIn || userLoggedInOffice) {
+    console.log("Great Employee.");   
+} else {
+    console.log("Need to change the Employee.");
+}
