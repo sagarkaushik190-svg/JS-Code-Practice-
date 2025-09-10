f22a46c0c10c752726e0b8dd3140808c5b031a6d
sagarkaushik190-svg <sagarkaushik190@gmail.com>
2025-09-10 08:56:06 +0000

    switch

---
+++ 04_controlFlow/switch.js
@@ -0,0 +1,17 @@
+let month = "January";
+
+switch (month) {
+    case 1:
+        console.log("Feb");        
+        break;
+    case 2:
+        console.log("January");        
+        break;
+    case 3:
+        console.log("December");        
+        break;
+
+    default:
+        console.log("default statement");      
+        break;
+}
