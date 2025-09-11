447817ae1614bf4d213b35982c713a25b211f411
sagarkaushik190-svg <sagarkaushik190@gmail.com>
2025-09-11 08:44:00 +0000

    loop

---
+++ 05_iteration/forloop.js
@@ -0,0 +1,20 @@
+// for(let index = 1; index < 10; index++){
+//     console.log(index);    
+// }
+
+
+// let myArray = ["Sagar","Taniya", "Harshita"];
+// for (let index = 0; index < myArray.length; index++) {
+//     const element = myArray[index];
+//     console.log(element);
+    
+// }
+
+for (let x = 1; x <= 10; x++){
+    if(x == 5){
+        console.log("5 is done");
+        // break;                   // It break the loop.
+        continue;                  // It break the particuler condition and continue the rest.  
+    }
+    console.log(x);  
+}
