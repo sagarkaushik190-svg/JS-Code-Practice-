ebfbeea132ac515e03890f38ad6e861d7d7a7dea
sagarkaushik190-svg <sagarkaushik190@gmail.com>
2025-09-12 13:17:05 +0000

    while

---
+++ 05_iteration/whileDowhile.js
@@ -0,0 +1,23 @@
+// let score = 0;
+// while (score < 10) {
+//     console.log(score);
+//     i = score + 1;
+// }
+
+//**********************************While loop with array.********************** */
+// let myArray = ["Sagar", "Harshita", "Taniya", "Rahul"];
+// let arrIndex = 0;
+// while (arrIndex < myArray.length){
+//     console.log(`Array elements is: ${myArray[arrIndex]} and the index value is ${arrIndex}`);    
+//     arrIndex++;
+// }
+
+
+//******************* */ Do while loop. *******************
+
+let x = 0;
+do{
+    console.log(x);
+    x++;
+
+} while(x < 10);
